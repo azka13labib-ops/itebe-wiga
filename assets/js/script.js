9d0ed66b3b1dafc05099b8e625c2e25b592c4cf6
azka13labib-ops <azka13labib@gmail.com>
2025-12-07 21:33:45 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+/* =======================================================================
+  COPYRIGHT (c) 2025 - PANITIA DIES NATALIS 38 ITB WIDYA GAMA LUMAJANG
+  Lomba Web Design & Development (SMARTVERSE 2025)
+  
+  ⚠️ PERINGATAN PENTING:
+  DILARANG MENGHAPUS KOMENTAR INI.
+  File ini adalah bagian dari Starter Pack resmi lomba.
+=======================================================================
+*/
+
+
+// Starter JavaScript
+console.log("Project berhasil dimuat!");
+
+// Contoh seleksi elemen menggunakan prefix class wajib
+const pageTitle = document.querySelector('.wg38_page_title');
+
+if(pageTitle) {
+    console.log("Judul halaman ini adalah: " + pageTitle.innerText);
+}
+
+// Silakan tambahkan logika interaktifitas di bawah ini
